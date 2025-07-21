58ab1bbfebd4ba0aa371c34f419b77bf9df6a06b
Yaroslav-Kyselov <mmmyaroslav@gmail,com>
2025-07-21 20:47:48 +0300

    clear START

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import './App.css'
-import MyComponets from "./MyComonents/myComponets.tsx";
 
 function App() {
 
   return (
     <>
-        <MyComponets text={"Hello 1"}/>
-        <MyComponets text={"Hello 2"}/>
-        <MyComponets text={"Hello 3"}/>
-        <MyComponets text={"Hello 4"}/>
-        {/*{MyComponets({text:'Hello 2'})}*/}
+
     </>
   );
 }
