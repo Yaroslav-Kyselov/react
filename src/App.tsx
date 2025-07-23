960dbc2ddb973cabc0a2ced0cb8c57962c332df7
Yaroslav-Kyselov <mmmyaroslav@gmail,com>
2025-07-23 20:49:36 +0300

    lessons/lesson1/map

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import './App.css'
+import {products} from "./data/ProductsList.ts";
+import type {IProduct} from "./models/IProduct.ts";
+
 
 function App() {
 
+
   return (
     <>
+      {
+      products.map((product:IProduct, index:number) => (
+      <div>
+        <h2>{product.title}</h2>
+        <p>Price: {product.price} $</p>
+        <img src={product.image} alt={product.title} style={{width: '200px'}}/>
+        <hr/>
 
-    </>
+
+      </div>))
+      }
+      </>
   );
 }
 
