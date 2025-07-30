52c359dd9659ea5f7531c28e389a5907466f5423
Yaroslav-Kyselov <mmmyaroslav@gmail,com>
2025-07-30 20:07:26 +0300

    task_kLZQR7Q98 midl

--- src/сomonents/CourseComponent/CourseComponent.tsx
+++ src/сomonents/CourseComponent/CourseComponent.tsx
@@ -1,4 +1,3 @@
-import './MyCourses.css'
 import type {ICourses} from "../../models/ICourses.tsx";
 
 interface CourseComponentProps {
@@ -6,19 +5,19 @@ interface CourseComponentProps {
     children: React.ReactNode
 }
 
-export const CourseComponent = ({item, children}: CourseComponentProps, index:number) => {
+export const CourseComponent = ({item}: CourseComponentProps, index: number) => {
     return (
         <div key={index} className='my-2 border-4'>
-                             <h2>{item.title}</h2>
-                            <p>Duration: {item.monthDuration} months</p>
-                            <p>Hours: {item.hourDuration}</p>
-                            <h3 className='my-10'>Modules:</h3>
-                            <ul >
-                                {item.modules.map((module, idx) => (
-                        <li key={idx}>{module}</li>
-                    ))}
-                 </ul>
-             </div>
+            <h2>{item.title}</h2>
+            <p>Duration: {item.monthDuration} months</p>
+            <p>Hours: {item.hourDuration}</p>
+            <h3 className='my-10'>Modules:</h3>
+            <ul>
+                {item.modules.map((module, idx) => (
+                    <li key={idx}>{module}</li>
+                ))}
+            </ul>
+        </div>
     )
 
 
--- src/сomonents/MyCoursesComponent/MyCoursesComponent.tsx
+++ src/сomonents/MyCoursesComponent/MyCoursesComponent.tsx
@@ -1,5 +1,16 @@
+import {coursesArray} from "../../data/arrays-2.ts";
+import {CourseComponent} from "../CourseComponent/CourseComponent.tsx";
+
 export const MyCoursesComponent = () => {
     return (
+        <div>
+            {
+                coursesArray.map((value, index:number) => <CourseComponent key={index} item={value}>
+
+                </CourseComponent>)
+            }
+
+        </div>
 
     )
 }
