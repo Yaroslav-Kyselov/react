4f6728425ce1797651769cc7605f791658d96e05
Yaroslav-Kyselov <mmmyaroslav@gmail,com>
2025-08-04 21:13:37 +0300

    lesson2 env

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,7 +1,14 @@
 import type {IUser} from "../models/IUser.tsx";
 
+
 export const getUsers = async   ():Promise<IUser[]> => {
-    const users = await fetch('https://jsonplaceholder.typicode.com/users')
+    const users = await fetch(import.meta.env.VITE_API_URL)
         .then(value => value.json());
     return users;
+}
+
+export const getUser = async   (id:string):Promise<IUser[]> => {
+    return  await fetch(import.meta.env.VITE_API_URL+'/'+id)
+        .then(value => value.json());
+
 }
