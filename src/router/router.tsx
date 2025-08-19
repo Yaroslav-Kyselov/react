7272aeb13fef42166e5a86acd778225665936aab
Yaroslav-Kyselov <mmmyaroslav@gmail,com>
2025-08-19 19:33:00 +0300

    tntlwuxFxI5 start base route structure

---
+++ src/router/router.tsx
@@ -0,0 +1,11 @@
+import {createBrowserRouter} from "react-router";
+import {MainLayout} from "../layouts/MainLayout.tsx";
+
+
+
+export const routes = createBrowserRouter([
+
+    {
+        path: "/", element: <MainLayout/>
+    }
+])
