04bdd1b0dd8df8c38982b9053896d5ab236c8378
Yaroslav-Kyselov <mmmyaroslav@gmail,com>
2025-08-29 17:24:09 +0300

    fin

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,11 +1,10 @@
 import {UsersComponent} from "../components/users-component/UsersComponent.tsx";
-import {Outlet} from "react-router/internal/react-server-client";
 
 export const UsersPage = () => {
     return (
         <div>
             <hr/>
-            <Outlet/>
+            {/*<Outlet/>*/}
             <hr/>
 
             <UsersComponent/>
