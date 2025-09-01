4cecb1f4be16149079ff2c3b429e2ac1819dad5f
Yaroslav-Kyselov <mmmyaroslav@gmail,com>
2025-09-01 19:15:20 +0300

    path1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import './App.css'
+import {PaginationComponent} from "./components/pagination/PaginationComponent.tsx";
+import {SomeComponent} from "./components/some/SomeComponent.tsx";
 
 function App() {
 
-  return (
-    <>
+    return (
+        <div>
+            <SomeComponent/>
+            hello world
+            <PaginationComponent/>
 
-
-    </>
-  );
+        </div>
+    );
 }
 
 export default App
---
+++ src/components/some/SomeComponent.tsx
@@ -0,0 +1,26 @@
+import {useEffect} from "react";
+import {useSearchParams} from "react-router";
+
+
+export const SomeComponent = () => {
+
+    const [query,setQuery]= useSearchParams()
+    useEffect(() => {
+         const pg = query.get('pg');
+            fetch('https://reqres.in/api/users?page='+pg)
+                .then(value => value.json())
+                .then(values => {
+                    console.log(values);
+                })
+
+    }, [query]);
+
+
+
+
+    return (
+        <div>
+            some content
+        </div>
+    );
+};
