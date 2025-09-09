6430ce9c1278446edebe638111b015856d5310e0
Yaroslav-Kyselov <mmmyaroslav@gmail,com>
2025-09-09 19:49:15 +0300

    fin

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -1,11 +1,47 @@
-import  React from 'react';
+import {type FormEvent, useState} from "react";
+
+interface IFormProps {
+    username: string;
+    password: string;
+}
 
 const FormComponent = () => {
+
+
+    const [formState, setFormState] = useState<IFormProps>({
+        username: 'foobar',
+        password: '1111'
+    })
+
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const user = {
+            username: formState.username,
+            password: formState.password
+        }
+        console.log(user)
+    };
+
+
+    const handleUsernameChange = (e: FormEvent<HTMLInputElement>) => {
+        const input: HTMLInputElement = e.target as HTMLInputElement
+        console.log(input.value)
+        setFormState({...formState, username: input.value})
+    }
+
+    const handlePasswordChange = (e: FormEvent<HTMLInputElement>) => {
+        const input: HTMLInputElement = e.target as HTMLInputElement
+        console.log(input.value)
+        setFormState({...formState, password: input.value})
+    }
+
     return (
         <div>
-            <form>
-                <input type="text" name={"username"}/>
-                <input type="text" name={"password"}/>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name={"username"} value={formState.username} onChange={handleUsernameChange}/>
+                <input type="text" name={"password"} value={formState.password} onChange={handlePasswordChange}/>
+                <button>send</button>
 
             </form>
         </div>
