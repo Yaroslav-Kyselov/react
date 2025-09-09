dd25256851135fc4f77cf082fc7ffb9e58fc0a09
Yaroslav-Kyselov <mmmyaroslav@gmail,com>
2025-09-09 21:26:13 +0300

    start

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -1,5 +1,5 @@
 import {useForm} from "react-hook-form";
-import React from "react";
+
 
 export interface IFormProps {
     username: string;
@@ -23,23 +23,23 @@ const FormComponent = () => {
             <form onSubmit={handleSubmit(customHandler)}>
 
                 <input type="text" {...register("username",
-                options:{
-                     required:true,
-                     minLength:{value: 3, message: "Min length is 3"}
-                })}/>
+                    {
+                        required: true,
+                        minLength: {value: 3, message: "Min length is 3"}
+                    })}/>
                 <input type="text" {...register("password",
-                options:{
-                    required:true,
-                    minLength:{value: 6, message: "Min length is 6"},
-                    maxLength:{value: 12, message: "Max length is 12"}
-                })}/>
+                    {
+                        required: true,
+                        minLength: {value: 6, message: "Min length is 6"},
+                        maxLength: {value: 12, message: "Max length is 12"}
+                    })}/>
                 <input type="number" {...register("age",
-                options:{
-                    required:true,
-                    min:{value: 3, message: "Min age is 3"},
-                    max:{value: 9, message: "Max age is 9"}
-                    valueAsNumber:true
-                })}/>
+                    {
+                        required: true,
+                        min: {value: 3, message: "Min age is 3"},
+                        max: {value: 9, message: "Max age is 9"},
+                        valueAsNumber: true
+                    })}/>
                 <button>send</button>
 
             </form>
