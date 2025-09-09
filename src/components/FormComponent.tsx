2e77a647b6ae78270c3612fe2ae7776708a8a794
Yaroslav-Kyselov <mmmyaroslav@gmail,com>
2025-09-09 20:37:52 +0300

    start

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -1,4 +1,5 @@
 import {useForm} from "react-hook-form";
+import React from "react";
 
 export interface IFormProps {
     username: string;
@@ -8,7 +9,11 @@ export interface IFormProps {
 
 const FormComponent = () => {
 
-    const {handleSubmit, register} = useForm<IFormProps>();
+    const {
+        handleSubmit,
+        register,
+        formState: {errors, isValid}
+    } = useForm<IFormProps>();
 
     const customHandler = (formDataProps: IFormProps) => {
         console.log(formDataProps);
@@ -17,9 +22,24 @@ const FormComponent = () => {
         <div>
             <form onSubmit={handleSubmit(customHandler)}>
 
-                <input type="text" {...register("username")}/>
-                <input type="text" {...register("password")}/>
-                <input type="number" {...register("age")}/>
+                <input type="text" {...register("username",
+                options:{
+                     required:true,
+                     minLength:{value: 3, message: "Min length is 3"}
+                })}/>
+                <input type="text" {...register("password",
+                options:{
+                    required:true,
+                    minLength:{value: 6, message: "Min length is 6"},
+                    maxLength:{value: 12, message: "Max length is 12"}
+                })}/>
+                <input type="number" {...register("age",
+                options:{
+                    required:true,
+                    min:{value: 3, message: "Min age is 3"},
+                    max:{value: 9, message: "Max age is 9"}
+                    valueAsNumber:true
+                })}/>
                 <button>send</button>
 
             </form>
