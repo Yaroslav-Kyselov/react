abeca4b60bef5b1647e17d4b92721c893910a34a
Yaroslav-Kyselov <mmmyaroslav@gmail,com>
2025-09-10 20:34:40 +0300

    fin

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -13,7 +13,9 @@ const FormComponent = () => {
         handleSubmit,
         register,
         formState: {errors, isValid}
-    } = useForm<IFormProps>();
+    } = useForm<IFormProps>({
+        mode: "all"
+    });
 
     const customHandler = (formDataProps: IFormProps) => {
         console.log(formDataProps);
@@ -21,26 +23,34 @@ const FormComponent = () => {
     return (
         <div>
             <form onSubmit={handleSubmit(customHandler)}>
-
-                <input type="text" {...register("username",
-                    {
-                        required: true,
-                        minLength: {value: 3, message: "Min length is 3"}
-                    })}/>
-                <input type="text" {...register("password",
-                    {
-                        required: true,
-                        minLength: {value: 6, message: "Min length is 6"},
-                        maxLength: {value: 12, message: "Max length is 12"}
-                    })}/>
-                <input type="number" {...register("age",
-                    {
-                        required: true,
-                        min: {value: 3, message: "Min age is 3"},
-                        max: {value: 9, message: "Max age is 9"},
-                        valueAsNumber: true
-                    })}/>
-                <button>send</button>
+                <label>
+                    <input type="text" {...register("username",
+                        {
+                            required: {value: true, message: "This field is required"},
+                            minLength: {value: 3, message: "Min length is 3"}
+                        })}/>
+                    {errors.username && <div>{errors.username.message}</div>}
+                </label>
+                <label>
+                    <input type="text" {...register("password",
+                        {
+                            required: true,
+                            minLength: {value: 6, message: "Min length is 6"},
+                            maxLength: {value: 12, message: "Max length is 12"}
+                        })}/>
+                    {errors.password && <div>{errors.password.message}</div>}
+                </label>
+                <label>
+                    <input type={"number"} {...register("age",
+                        {
+                            required: true,
+                            min: {value: 3, message: "Min age is 3"},
+                            max: {value: 117, message: "Max age is 117"},
+                            valueAsNumber: true
+                        })}/>
+                    {errors.age && <div>{errors.age.message}</div>}
+                </label>
+                <button disabled={!isValid}>send</button>
 
             </form>
         </div>
