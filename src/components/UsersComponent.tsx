7e76b84c8d891c5947d6f569a9eaae1fbfa5f8ba
Yaroslav-Kyselov <mmmyaroslav@gmail,com>
2025-09-26 08:44:19 +0300

    fin useCallback

--- src/components/UsersComponent.tsx
+++ src/components/UsersComponent.tsx
@@ -1,9 +1,14 @@
 import {UserComponent} from "./UserComponent.tsx";
-import {useEffect, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 
 export const UsersComponent = () => {
     console.log("users")
     const [users, setUsers] = useState([])
+
+    const foo = useCallback(() => {
+        console.log('foo test')
+    }, [])
+
     useEffect(() => {
         fetch("https://jsonplaceholder.typicode.com/users")
             .then(response => response.json())
@@ -11,9 +16,9 @@ export const UsersComponent = () => {
     }, []);
     return (
         <div>
-            UsersComponents
+            UsersComponent
 
-            <UserComponent/>
+            <UserComponent foo={foo}/>
         </div>
     );
 };
