29e640cec0b0da48652b316ab19cd40ed8b81a32
Yaroslav-Kyselov <mmmyaroslav@gmail,com>
2025-09-26 08:53:27 +0300

    fin useMemo

--- src/components/UserComponent.tsx
+++ src/components/UserComponent.tsx
@@ -1,8 +1,9 @@
 import {type FC, memo} from "react";
 
-export const UserComponent:FC<{foo:()=>void}> = memo(() => {
+export const UserComponent: FC<{ foo: () => void, arr: number[] }> = memo(({arr}) => {
 
     console.log("user")
+    console.log(arr)
     return (
         <div>
             UserComponent
--- src/components/UsersComponent.tsx
+++ src/components/UsersComponent.tsx
@@ -1,9 +1,13 @@
 import {UserComponent} from "./UserComponent.tsx";
-import {useCallback, useEffect, useState} from "react";
+import {useCallback, useEffect, useMemo, useState} from "react";
 
 export const UsersComponent = () => {
     console.log("users")
     const [users, setUsers] = useState([])
+
+    const arr:number[]=useMemo(() => {
+        return [11,22,33]
+    }, []);
 
     const foo = useCallback(() => {
         console.log('foo test')
@@ -18,7 +22,7 @@ export const UsersComponent = () => {
         <div>
             UsersComponent
 
-            <UserComponent foo={foo}/>
+            <UserComponent foo={foo} arr={arr}/>
         </div>
     );
 };
